e6e9f0dcdad9448f0f8e1b1ddae9b8a294dad823
abigpotostew <stew.bracken@gmail.com>
2013-01-24 19:59:37 -0800

    almost got our custom tiles working woo

--- ProjectSunlight/main.lua
+++ ProjectSunlight/main.lua
@@ -32,32 +32,45 @@ local options = { frames = {},
 	sheetContentWidth = 1024,
 	sheetContentHeight = 1024, 
 }
-local frameWidth = 32
-local frameHeight = 32
-for i=0,32 do
-	table.insert(options.frames,{x=32*i,y=0,width=frameWidth,height=frameHeight})
+local sequences
+local sprites
+local tileWidth = 32
+local tileHeight = 32
+local columns = 2
+local rows	  = 3
+for iY=0,rows do
+	for jX=0,columns do
+		table.insert(options.frames,{ x = jX*tileWidth,
+									  y = iY*tileHeight,
+									  width = tileWidth,
+									  height = tileHeight
+									 })
+	end
 end
+-- for i=0,32 do
+-- 	table.insert(options.frames,{x=32*i,y=0,width=tileWidth,height=tileHeight})
+-- end
 
 -- (2) Generate the remaining 256 frames programatically:
 -- Instead of typing them all out, we just duplicate the first row 15 more times,
 -- adjusting the y value as necessary.
-local frames = options.frames
-for j=1,30 do
-	for i=1,32 do
-		local src = frames[i]
-		local element = {
-			x = src.x,
-			y = 32 * j,
-			width = src.width,
-			height = src.height,
-		}
-		table.insert( frames, element )
-	end
-end
+--local frames = options.frames
+-- for j=1,30 do
+-- 	for i=1,32 do
+-- 		local src = frames[i]
+-- 		local element = {
+-- 			x = src.x,
+-- 			y = 32 * j,
+-- 			width = src.width,
+-- 			height = src.height,
+-- 		}
+-- 		table.insert( frames, element )
+-- 	end
+-- end
 
-local sheet = graphics.newImageSheet( "dancers.png", options )
+local sheet = graphics.newImageSheet( "tiles.png", options )
 
-local sequenceData = {}
+--local sequenceData = {}
 
 local w = 32
 local h = 32
@@ -67,37 +80,69 @@ local halfH = h*0.5
 local function createTiles( x, y, xMax, yMax, group )
 	local xStart = x
 	local j = 0
-	while ( true ) do
-		local i = 1+math.fmod( j, 32 )
-		j = j + 1
-		
-		local dancer = "dancer" .. i
-		local numFrames = 32
-		local start = (i % 32)*numFrames + 1
-		local sequence = { name=dancer, start=start, count=numFrames, loopDirection="bounce" }
-
-		local sprite
-
-		if ( group ) then
-			sprite = display.newSprite( sheet, sequence )
-			group:insert( sprite )
-		else
-			sprite = display.newSprite( sheet, sequence )
-		end
-
-		sprite:translate( x, y )
-		sprite:play()
-
-		x = x + w
-		if ( x > xMax ) then
-			x = xStart
-			y = y + h
-		end
-
-		if ( y > yMax ) then
-			break
+	
+	sequences = {}
+	sequences["wood"] = {name="wood", start=1, count = 1}
+	sequences["grass"] = {name="grass", start=2, count = 1}
+	sequences["horz"] = {name="horz", start=3, count = 1}
+	sequences["leftdown"] = {name="leftdown", start=4, count = 1}
+	sequences["rightdown"] = {name="rightdown", start=5, count = 1}
+	sequences["stone"] = {name="stone", start=6, count = 1}
+	sequences["leftup"] = {name="leftup", start=7, count = 1}
+	sequences["rightup"] = {name="rightup", start=8, count = 1}
+	sequences["vert"] = {name="vert", start=9, count = 1}
+	sequences["water"] = {name="water", start=10, count = 1}
+	
+	for iX = 1,tileWidth do
+		for jY = 1, tileHeight do
+			local sprite = display.newSprite(sheet, sequences.water )
+			group:insert(sprite)
+			sprite:translate(iX*tileWidth,jY*tileHeight)
+			sprite:play()
 		end
 	end
+	
+	-- for k,v in pairs(sequences) do
+	-- 	local sprite = display.newSprite( sheet, v )
+	-- 	if ( group ) then
+	-- 		group:insert( sprite )
+	-- 	end
+	-- 	sprite:translate( options.frames[i].x, options.frames[1].y )
+	-- 	sprite:play()
+	-- 	table.insert(sprites,sprite)
+	-- end
+
+	-- while ( true ) do
+	-- 		local i = 1+math.fmod( j, 32 )
+	-- 		j = j + 1
+	-- 		
+	-- 		local dancer = "dancer" .. i
+	-- 		local numFrames = 32
+	-- 		local start = (i % 32)*numFrames + 1
+	-- 		local sequence = { name=dancer, start=start, count=numFrames, loopDirection="bounce" }
+	-- 
+	-- 		local sprite
+	-- 
+	-- 		if ( group ) then
+	-- 			sprite = display.newSprite( sheet, sequence )
+	-- 			group:insert( sprite )
+	-- 		else
+	-- 			sprite = display.newSprite( sheet, sequence )
+	-- 		end
+	-- 
+	-- 		sprite:translate( x, y )
+	-- 		sprite:play()
+	-- 
+	-- 		x = x + w
+	-- 		if ( x > xMax ) then
+	-- 			x = xStart
+	-- 			y = y + h
+	-- 		end
+	-- 
+	-- 		if ( y > yMax ) then
+	-- 			break
+	-- 		end
+	-- 	end
 
 end
 
