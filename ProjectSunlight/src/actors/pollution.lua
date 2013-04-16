dcb6d6e1836458a5b831b0b363ac79d185d9d3d0
abigpotostew <stew.bracken@gmail.com>
2013-04-15 18:36:06 -0700

    Onwards perri!

--- ProjectSunlight/src/actors/pollution.lua
+++ ProjectSunlight/src/actors/pollution.lua
@@ -7,7 +7,8 @@ Ammo:makeInit(function(class, self, pollutionType)
 		self.typeInfo = pollutionType
 		self.hitCount = 10
 		
-		
+		self:createSprite("normal", trajectory.start.position.x, trajectory.start.position.y,
+		self.typeInfo.scale, self.typeInfo.scale)
 		
 		self:addPhysics()
 		
